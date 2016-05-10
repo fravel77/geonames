e7696672e4ca885ed55ad53b9977ca35d3f31c69
Arber Dervishi <arbdervishi@gmail.com>
2016-05-10 15:09:16 +0200

    fix migrations file names (missing _table)

---
+++ src/migrations/2013_11_28_173425_create_geonames_admin_subdivisions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGeonamesAdminSubdivisionsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('geonames_admin_subdivisions', function(Blueprint $table)
+		{
+			$table->string('code', 15)->index();
+			$table->text('name');
+			$table->text('name_ascii');
+			$table->integer('name_id')->index();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('geonames_admin_subdivisions');
+	}
+
+}
---
+++ src/migrations/2013_11_28_173936_create_geonames_timezones_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGeonamesTimezonesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('geonames_timezones', function(Blueprint $table)
+		{
+			$table->string('id', 200)->index();
+			$table->decimal('gmt_offset', 3, 1);
+			$table->decimal('dst_offset', 3, 1);
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('geonames_timezones');
+	}
+
+}
