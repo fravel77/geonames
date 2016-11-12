f30b69c531666ada5402a1285d62ed3acb9fe450
leszek <leszek.haim@gmail.com>
2016-11-12 17:55:36 +0000

    fix migration class name CreateGeonamesPostalCodesTable

--- src/migrations/2016_10_10_123456_create_geonames_postal_codes_table.php
+++ src/migrations/2016_10_10_123456_create_geonames_postal_codes_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePostalCodesTable extends Migration {
+class CreateGeonamesPostalCodesTable extends Migration {
 
 	/**
 	 * Run the migrations.
