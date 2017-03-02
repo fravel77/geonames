5cd6dd52ea3f52bdd1d5b4cdf340201bc8846005
fravel77 <fravel77@gmail.com>
2017-03-02 22:05:05 +0100

    Update 2013_11_28_170337_create_geonames_names_table.php

--- src/migrations/2013_11_28_170337_create_geonames_names_table.php
+++ src/migrations/2013_11_28_170337_create_geonames_names_table.php
@@ -22,13 +22,13 @@ class CreateGeonamesNamesTable extends Migration {
 			$table->decimal('longitude', 10, 7)->comment('longitude in decimal degrees (wgs84)');
 			$table->string('f_class', 1)->comment('see http://www.geonames.org/export/codes.html, char(1)');
 			$table->string('f_code', 10)->comment('see http://www.geonames.org/export/codes.html, varchar(10)');
-			$table->char('country_id', 2)->index()->comment('ISO-3166 2-letter country code, 2 characters');
+			$table->string('country_id', 2)->index()->comment('ISO-3166 2-letter country code, 2 characters');
 			$table->string('cc2', 200)->comment('alternate country codes, comma separated, ISO-3166 2-letter country code, 200 characters');
 			$table->string('admin1', 20)->index()->comment('fipscode (subject to change to iso code), see exceptions below, see file admin1Codes.txt for display names of this code; varchar(20)');
 			$table->string('admin2', 80)->index()->comment('code for the second administrative division, a county in the US, see file admin2Codes.txt; varchar(80)');
 			$table->string('admin3', 20)->index()->comment('code for third level administrative division, varchar(20)');
 			$table->string('admin4', 20)->index()->comment('code for fourth level administrative division, varchar(20)');
-			$table->decimal('population', 11, 0)->index()->comment('bigint (8 byte int)');
+			$table->integer('population')->index()->comment('bigint (8 byte int)');
 			$table->smallInteger('elevation')->unsigned()->comment('in meters, integer');
 			$table->smallInteger('gtopo30')->unsigned()->comment("digital elevation model, srtm3 or gtopo30, average elevation of 3''x3'' (ca 90mx90m) or 30''x30'' (ca 900mx900m) area in meters, integer. srtm processed by cgiar/ciat.");
 			$table->string('timezone_id', 40)->index()->comment('the timezone id (see file timeZone.txt) varchar(40)');
